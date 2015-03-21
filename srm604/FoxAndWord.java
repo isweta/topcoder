037d70d5e3a28a0798c2f27fe78abf3b879bbe10
isweta <mailbox.sweta@gmail.com>
2015-03-21 10:14:08 -0700

    slight error

--- srm604/FoxAndWord.java
+++ srm604/FoxAndWord.java
@@ -12,13 +12,15 @@ public class FoxAndWord
 	public static int howManyPairs(String[] words)
 	{
 		int pair_count=0;
-		for(int curr_wd_ind=0; curr_wd_ind<words.length; curr_wd_ind++)
+		for(int curr_wd_ind=0; curr_wd_ind<words.length-1; curr_wd_ind++)
 		{
 			String curr_wd=words[curr_wd_ind];
 			for(int pair_wd_ind=curr_wd_ind+1; pair_wd_ind<words.length; pair_wd_ind++)
 			{
 				
 				String pair_wd=words[pair_wd_ind];
+				if(curr_wd.length()!=pair_wd.length())
+					break;
 				for(int b_start=1; b_start<curr_wd.length(); b_start++)
 				{
 					String pref_curr=curr_wd.substring(0, b_start);
